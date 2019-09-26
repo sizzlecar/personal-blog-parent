8d1f89f1c41322126ec27d65d0a372f6c7f086b0
763667966@qq.com <ting876587134>
2019-09-27 01:21:09 +0800

    添加过滤器中间件

--- app/middleware/filter.js
+++ app/middleware/filter.js
@@ -1,7 +1,38 @@
-module.exports = options => {
-    return async function gzip(ctx, next) {
-        await next();
+'use strict';
 
-        // 后续中间件执行完成后将响应体转换成 gzip
-    };
+const jwt = require('jsonwebtoken');
+const privateKey = 'jinwandalaohu';
+module.exports = options => {
+  return async function filter(ctx, next) {
+    await next();
+    const result = ctx.app.config.baseResult;
+    result.data = null;
+    const header = ctx.request.header;
+    ctx.logger.info(header);
+    const tokenStr = header.authorization;
+    ctx.logger.info(tokenStr);
+    if (tokenStr) {
+      const token = tokenStr.replace('Bearer ', '');
+      jwt.verify(token, privateKey, function(err, decoded) {
+        if (err) {
+          if (err.name === 'TokenExpiredError') {
+            result.message = 'token已过期，请重新登陆！';
+            ctx.body = result;
+          } else if (err.name === 'JsonWebTokenError') {
+            result.message = 'token错误';
+            ctx.body = result;
+          } else if (err.name === 'NotBeforeError') {
+            result.message = 'jwt not active';
+            ctx.body = result;
+          }
+        }
+        ctx.logger.info(decoded);
+      });
+    } else {
+      result.message = '请登陆';
+      result.code = 'E0004';
+      ctx.body = result;
+    }
+
+  };
 };
--- app/router.js
+++ app/router.js
@@ -6,9 +6,10 @@
 module.exports = app => {
   const { router, controller } = app;
   app.logger.info('controller: %j', controller);
+  const filter = app.middleware.filter();
   router.get('/', controller.home.index);
   router.get('/blog/blog-menu/list', controller.blogMenu.getAllMenu);
   router.get('/blog/list/:menuId', controller.blog.getBlogList);
-  router.get('/blog/detail/:menuId/:blogId', controller.blog.getBlogDetail);
+  router.get('/blog/detail/:menuId/:blogId', filter, controller.blog.getBlogDetail);
   router.post('/blog/user/login', controller.blogUser.login);
 };
